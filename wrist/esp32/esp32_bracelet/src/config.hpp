a6894e296732c29c8b54a674b814bccd778ec36e
Aleksei <46602330+AlexEra@users.noreply.github.com>
2022-11-23 14:54:23 +0300

    fix

--- wrist/esp32/esp32_bracelet/src/config.hpp
+++ wrist/esp32/esp32_bracelet/src/config.hpp
@@ -2,6 +2,7 @@
 
 #define DEVICE_ID           2 // 2 - металлический ремешок or 3 - резиновый ремешок (белый)
 
+#define AUTOTHRESHOLDING
 // #define MPU_TESTING
 // #define TEST_SIGNALS_READING
 // #define ANALOG_SIGNALS_PRINTING
